276b9e3009a8fed31d52f24869cdd73d19372325
Cesario_Neto <cesarioneto7@gmail.com>
2023-11-30 11:42:37 -0300

    create login and register views

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     # index
     path('', views.index, name='index'),
+
+    # account
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login_view'),
+    path('logout/', views.logout_view, name='logout_view'),
 ]
---
+++ frontend/views/account.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render, redirect
+from account.forms import RegisterForm, LoginForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            redirect('login_view')
+    else:
+        form = RegisterForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'account/pages/register.html', context=context)
+
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            form_email = form.data.get('email')
+            form_password = form.data.get('password')
+            user = authenticate(email=form_email, password=form_password)
+            if user:
+                login(request, user)
+                return redirect('index')
+            else:
+                messages.error(request, 'Email e/ou senha invalido')
+
+    else:
+        form = LoginForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'account/pages/login.html', context=context)
+
+
+@login_required(redirect_field_name='next', login_url='/login/')
+def logout_view(request):
+    logout(request)
+    return redirect('login_view')
