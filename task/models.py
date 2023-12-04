e32f534138ce08fed334b3f8e0da8d92fcaec6b8
Cesario_Neto <cesarioneto7@gmail.com>
2023-12-03 22:30:22 -0300

    changing save method

--- task/models.py
+++ task/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from core.models import BaseModel
 from django.utils.text import slugify
 from account.models import User
+import string as s
+import random
 
 
 class Task(BaseModel):
@@ -37,8 +39,12 @@ class Task(BaseModel):
     slug = models.SlugField(verbose_name='Slug')
 
     def save(self, *args, **kwargs):
+        characters = list(s.ascii_letters)
+        random.shuffle(characters)
+        random_characters = ''.join(
+            value for i, value in enumerate(characters) if i < 4)
         if not self.slug:
-            self.slug = slugify(self.title)
+            self.slug = f'{slugify(self.title)}{random_characters}'
 
         return super().save(*args, **kwargs)
 
